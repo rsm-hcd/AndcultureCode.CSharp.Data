ebfc284e1c993913f48cbc064d5fd601afed3ebb
Winton DeShong <contact@winton.me>
2020-08-03 08:22:39 -0400

    Added IDatabaseContext and IApplicationBuilder#ConfigureDatabase

--- src/AndcultureCode.CSharp.Data/AndcultureCode.CSharp.Data.md
+++ src/AndcultureCode.CSharp.Data/AndcultureCode.CSharp.Data.md
@@ -4,6 +4,9 @@
 ## Contents
 
 - [IApplicationBuilderExtensions](#T-AndcultureCode-CSharp-Data-Extensions-IApplicationBuilderExtensions 'AndcultureCode.CSharp.Data.Extensions.IApplicationBuilderExtensions')
+  - [ConfigureDatabase\`\`1(app,serviceProvider,migrate,seeds)](#M-AndcultureCode-CSharp-Data-Extensions-IApplicationBuilderExtensions-ConfigureDatabase``1-Microsoft-AspNetCore-Builder-IApplicationBuilder,System-IServiceProvider,System-Boolean,AndcultureCode-CSharp-Core-SeedsBase{``0}- 'AndcultureCode.CSharp.Data.Extensions.IApplicationBuilderExtensions.ConfigureDatabase``1(Microsoft.AspNetCore.Builder.IApplicationBuilder,System.IServiceProvider,System.Boolean,AndcultureCode.CSharp.Core.SeedsBase{``0})')
+- [IDatabaseContext](#T-AndcultureCode-CSharp-Data-Interfaces-IDatabaseContext 'AndcultureCode.CSharp.Data.Interfaces.IDatabaseContext')
+  - [Database](#P-AndcultureCode-CSharp-Data-Interfaces-IDatabaseContext-Database 'AndcultureCode.CSharp.Data.Interfaces.IDatabaseContext.Database')
 
 <a name='T-AndcultureCode-CSharp-Data-Extensions-IApplicationBuilderExtensions'></a>
 ## IApplicationBuilderExtensions `type`
@@ -14,4 +17,39 @@ AndcultureCode.CSharp.Data.Extensions
 
 ##### Summary
 
-Extensions for IApplicationBuilder
+Infrastructure Data IApplicationBuilder Extensions
+
+<a name='M-AndcultureCode-CSharp-Data-Extensions-IApplicationBuilderExtensions-ConfigureDatabase``1-Microsoft-AspNetCore-Builder-IApplicationBuilder,System-IServiceProvider,System-Boolean,AndcultureCode-CSharp-Core-SeedsBase{``0}-'></a>
+### ConfigureDatabase\`\`1(app,serviceProvider,migrate,seeds) `method`
+
+##### Summary
+
+Performs application startup related database configuration tasks
+
+##### Parameters
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| app | [Microsoft.AspNetCore.Builder.IApplicationBuilder](#T-Microsoft-AspNetCore-Builder-IApplicationBuilder 'Microsoft.AspNetCore.Builder.IApplicationBuilder') |  |
+| serviceProvider | [System.IServiceProvider](http://msdn.microsoft.com/query/dev14.query?appId=Dev14IDEF1&l=EN-US&k=k:System.IServiceProvider 'System.IServiceProvider') |  |
+| migrate | [System.Boolean](http://msdn.microsoft.com/query/dev14.query?appId=Dev14IDEF1&l=EN-US&k=k:System.Boolean 'System.Boolean') | Should outstanding code-first migrations be run? |
+| seeds | [AndcultureCode.CSharp.Core.SeedsBase{\`\`0}](#T-AndcultureCode-CSharp-Core-SeedsBase{``0} 'AndcultureCode.CSharp.Core.SeedsBase{``0}') | Optional instance of seeds to run |
+
+<a name='T-AndcultureCode-CSharp-Data-Interfaces-IDatabaseContext'></a>
+## IDatabaseContext `type`
+
+##### Namespace
+
+AndcultureCode.CSharp.Data.Interfaces
+
+##### Summary
+
+Database specific context
+
+<a name='P-AndcultureCode-CSharp-Data-Interfaces-IDatabaseContext-Database'></a>
+### Database `property`
+
+##### Summary
+
+Provides access to database related information and operations for this context.
+///
--- src/AndcultureCode.CSharp.Data/Extensions/IApplicationBuilderExtensions.cs
+++ src/AndcultureCode.CSharp.Data/Extensions/IApplicationBuilderExtensions.cs
@@ -1,17 +1,81 @@
+using System;
+using AndcultureCode.CSharp.Core;
 using AndcultureCode.CSharp.Core.Constants;
+using AndcultureCode.CSharp.Core.Interfaces;
+using AndcultureCode.CSharp.Data.Interfaces;
+using AndcultureCode.CSharpCore.Constants;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
-using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace AndcultureCode.CSharp.Data.Extensions
 {
     /// <summary>
-    /// Extensions for IApplicationBuilder
+    /// Infrastructure Data IApplicationBuilder Extensions
     /// </summary>
     public static class IApplicationBuilderExtensions
     {
+        #region Public Methods
 
+        /// <summary>
+        /// Performs application startup related database configuration tasks
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="serviceProvider"></param>
+        /// <param name="migrate">Should outstanding code-first migrations be run?</param>
+        /// <param name="seeds">Optional instance of seeds to run</param>
+        public static void ConfigureDatabase<TContext>(
+            this IApplicationBuilder app,
+            IServiceProvider serviceProvider,
+            bool migrate = true,
+            SeedsBase<TContext> seeds = null
+        )
+            where TContext : class, IDatabaseContext
+        {
+            var env = serviceProvider.GetService<IHostEnvironment>();
+            if (env.IsEnvironment(EnvironmentConstants.TESTING))
+            {
+                return;
+            }
+
+            var context = serviceProvider.GetService<TContext>();
+            var logger = serviceProvider.GetService<ILogger<IApplicationBuilder>>();
+
+            if (migrate)
+            {
+                Migrate(context.Database, logger);
+            }
+
+            if (seeds != null)
+            {
+                Seed(seeds, logger);
+            }
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private static void Migrate(DatabaseFacade database, ILogger<IApplicationBuilder> logger)
+        {
+            logger.LogInformation("Migrating database...");
+            database.SetCommandTimeout(int.MaxValue);
+            database.Migrate();
+            logger.LogInformation("Database migrated");
+        }
+
+        private static void Seed<TContext>(SeedsBase<TContext> seeds, ILogger<IApplicationBuilder> logger)
+            where TContext : class, IDatabaseContext
+        {
+            logger.LogInformation("Seeding database...");
+            seeds.Create();
+            logger.LogInformation("Database seeded");
+        }
+
+        #endregion Private Methods
     }
 }
